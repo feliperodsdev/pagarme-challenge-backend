9d165822f2554ca74515b10da1b5be19aaeaf4ca
liber <libertyisrequired@gmail.com>
2023-01-07 11:17:53 -0300

    feat: createTransactionService and test

---
+++ src/services/createTransaction.spec.ts
@@ -0,0 +1,31 @@
+import { describe, expect, it } from "vitest";
+import {
+  createTransaction,
+  ICreateTransactionRepository,
+} from "./createTransaction";
+import { enumCard, Transaction } from "../entities/Transaction";
+
+describe("Using creaTransactionService", () => {
+  it("Should return statusCode 201", async () => {
+    class respositoryTest implements ICreateTransactionRepository {
+      createTransaction(transaction: Transaction) {
+        console.log("");
+      }
+    }
+    const serviceCreate = new createTransaction(new respositoryTest());
+    const response = await serviceCreate.execute({
+      body: {
+        value: 100,
+        desc: "idk",
+        payment_method: enumCard.CREDIT_CARD,
+        expiration_date_card: "01-25",
+        cvv: 542,
+        owner_card: "felipe",
+        card_number: "5503550355031457",
+      },
+    });
+
+    expect(response.statusCode).toEqual(201);
+    expect(response.data).toEqual("Created");
+  });
+});
---
+++ src/services/createTransaction.ts
@@ -0,0 +1,76 @@
+import { HttpRequest, HttpResponse } from "../helpers";
+import { enumCard, Transaction } from "../entities/Transaction";
+
+interface CreateTransactionParams {
+  value: number;
+  desc: string;
+  payment_method: enumCard;
+  card_number: string;
+  owner_card: string;
+  expiration_date_card: string;
+  cvv: number;
+}
+
+interface IcreateTransactionService {
+  execute(
+    HttpRequest: HttpRequest<CreateTransactionParams>
+  ): Promise<HttpResponse<string>>;
+}
+
+export interface ICreateTransactionRepository {
+  createTransaction(transaction: Transaction): void;
+}
+
+export class createTransaction implements IcreateTransactionService {
+  constructor(
+    private readonly createTransactionRepository: ICreateTransactionRepository
+  ) {}
+  async execute(
+    HttpRequest: HttpRequest<CreateTransactionParams>
+  ): Promise<HttpResponse<string>> {
+    try {
+      if (!HttpRequest.body) {
+        return {
+          statusCode: 400,
+          data: "Body not found",
+        };
+      }
+
+      const requiredFields = [
+        "value",
+        "desc",
+        "payment_method",
+        "card_number",
+        "owner_card",
+        "expiration_date_card",
+        "cvv",
+      ];
+
+      for (const field of requiredFields) {
+        if (
+          !HttpRequest?.body?.[field as keyof CreateTransactionParams] ||
+          field.trim() == ""
+        ) {
+          return {
+            statusCode: 400,
+            data: `${field} is required`,
+          };
+        }
+      }
+
+      this.createTransactionRepository.createTransaction(
+        new Transaction(HttpRequest.body)
+      );
+
+      return {
+        statusCode: 201,
+        data: "Created",
+      };
+    } catch (e) {
+      return {
+        statusCode: 500,
+        data: "Server Error",
+      };
+    }
+  }
+}
