0c9e5aa57e5b562fdba992467552ec9397e8f170
liber <libertyisrequired@gmail.com>
2023-01-08 15:13:11 -0300

    feat: amount router

--- src/routers/transaction.ts
+++ src/routers/transaction.ts
@@ -1,6 +1,8 @@
 import express from "express";
-import { PrismaCreateTransaction } from "../respositories/PrismaCreateTransactionRespository";
+import { PrismaCreateTransaction } from "../respositories/Transaction/PrismaCreateTransactionAndPayableRespository";
+import { PrismaGetSaldo } from "../respositories/Transaction/PrismaGetSaldoRepository";
 import { createTransaction } from "../services/createTransaction";
+import { getSaldo } from "../services/getSaldo";
 export const transactionRouter = express.Router();
 
 transactionRouter.post("/create", async (req, res) => {
@@ -9,5 +11,15 @@ transactionRouter.post("/create", async (req, res) => {
     createTransactionRepository
   );
   const response = await createTransactionService.execute(req);
+
+  res.status(response.statusCode).send(response.data);
+});
+
+transactionRouter.get("/amount", async (req, res) => {
+  const getSaldoRepository = new PrismaGetSaldo();
+  const getSaldoService = new getSaldo(getSaldoRepository);
+
+  const response = await getSaldoService.execute();
+
   res.status(response.statusCode).send(response.data);
 });
